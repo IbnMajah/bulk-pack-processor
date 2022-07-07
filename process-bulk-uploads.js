736f6ae1433bb8bb2a79ba02ae8049f6e6838d47
Olojakpoke Daniel <holodja@hotmail.com>
2022-07-07 06:18:49 +0100

    fix: add process.exit(1) if there are failing bulk packs

--- process-bulk-uploads.js
+++ process-bulk-uploads.js
@@ -74,6 +74,7 @@ const v1Requests = async (data, endpoint, httpVerb) => {
     ORDER BY KEY ASC`,
   };
   const rval = await pool.query(query);
+  let failingBulkPacks = false;
 
   for (let row of rval.rows) {
     let shouldBeProcessed = true;
@@ -220,11 +221,15 @@ const v1Requests = async (data, endpoint, httpVerb) => {
     console.log("update");
     if (shouldBeProcessed) {
       await pool.query(update);
+    } else {
+      failingBulkPacks = true;
     }
     console.log(`Processed bulk tree upload ${row.id}`);
   }
   console.log("done");
-  pool.end();
+  await pool.end();
+
+  failingBulkPacks ? process.exit(1) : process.exit(0);
   process.exit(0);
 })().catch((e) => {
   console.log(e);
